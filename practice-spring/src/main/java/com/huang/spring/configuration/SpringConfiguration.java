c50abda38235b244b85ef0aa2cc9fa0bfc20455b
huang <huang_jiangling@163.com>
2017-07-12 10:32:57 +0800

    spring；

--- practice-spring/src/main/java/com/huang/spring/configuration/SpringConfiguration.java
+++ practice-spring/src/main/java/com/huang/spring/configuration/SpringConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
