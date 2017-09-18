6eab78b71d2d865b9f1a4fcb338a3b2d65dca2c0
huang <huangjiangling01@chinatopcredit.com>
2017-09-18 10:50:21 +0800

    timer；

---
+++ practice-spring/src/main/java/com/huang/spring/configuration/HuangBean.java
@@ -0,0 +1,15 @@
+package com.huang.spring.configuration;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by huang_jiangling on 2017/8/31.
+ */
+@Component
+public class HuangBean {
+
+    private String name = "huang";
+    private int age17;
+
+
+}
