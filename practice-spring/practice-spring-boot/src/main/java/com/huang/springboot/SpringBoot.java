295326057090a89a65f5b3bc07346f40d1239271
huang <huangjiangling01@chinatopcredit.com>
2018-02-06 13:49:58 +0800

    util;

---
+++ practice-spring/practice-spring-boot/src/main/java/com/huang/springboot/SpringBoot.java
@@ -0,0 +1,14 @@
+package com.huang.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by huang_jiangling on 2018/2/5.
+ */
+@SpringBootApplication
+public class SpringBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot.class, args);
+    }
+}
